ae309fdb7f22ee041cca1c04c47bc5ad1be83adf
balazsborjan <balazs.borjan@gmail.com>
2017-04-24 18:51:28 +0200

    First commit

---
+++ Project/Telemedicina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telemedicina
+//
+//  Created by Balázs Bojrán on 2017. 04. 24..
+//  Copyright © 2017. SZTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
